99c2d41419fb81043509e0deae096b1a2654896c
Alexander Koskovich <zvnexus@outlook.com>
2020-03-06 17:02:17 -0500

    PRISM
    Change-Id: Ie3cb469b3d168f1cc954dd835e977152082840fc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
-#
 # Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2020 Prism
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_oneplus3.mk
+    $(LOCAL_DIR)/prism_oneplus3.mk
